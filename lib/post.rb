e7fe7e487cc3717b176f58899d540e014f86cb77
Tao <werewaswas@live.com>
2018-07-19 21:58:48 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,12 +2,13 @@ class Post
   attr_accessor :title, :author
 
   @@all = []
-  
+
   def initialize(title)
   end
 
   def author_name
-    @author.name
+    
+    @author ? @author.name : nil
   end
-  
+
 end
