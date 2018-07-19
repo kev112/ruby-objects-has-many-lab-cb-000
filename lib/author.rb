8e854e957df704a9bb7851367a84354844d81cc3
Tao <werewaswas@live.com>
2018-07-19 22:10:34 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,12 +8,19 @@ class Author
     @posts = []
   end
 
-  def add_post
+  def add_post(post)
+    @posts << post
+    @@posts << post
+  end
+
+  def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
     @posts << post
     @@posts << post
   end
+
+
 
   def self.post_count
     @@posts.length
